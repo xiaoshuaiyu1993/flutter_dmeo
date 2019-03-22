f99effc7241c65398e774fdc0c928918d11c4c10
xiaoshuaiyu1993 <xiaoshuaiyu1993@163.com>
2019-03-22 17:06:06 +0800

    ListView组件的使用

--- lib/main.dart
+++ lib/main.dart
@@ -8,25 +8,24 @@ class MyWidget extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Flutter"),
+          title: Text(
+            "Flutter",
+            style: TextStyle(color: Colors.white),
+          ),
+          elevation: 0.0, // 设置导航栏下面的阴影大小
         ),
-        body: Hello(),
+        body: ListView.builder(
+          itemCount: 10, // 列表总条数
+          itemBuilder: (BuildContext context, int index) {
+            return Text(
+              "$index",
+              style: TextStyle(fontSize: 25.0, color: Colors.red),
+            );
+          }, // 每条数据对应的widget
+        ),
       ),
       // 设置APP主题
       theme: ThemeData(primaryColor: Colors.lightBlue),
     );
-  }
-}
-
-class Hello extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-        child: Text(
-      "Hello, world!",
-      textDirection: TextDirection.ltr,
-      style: TextStyle(
-          fontSize: 60.0, fontWeight: FontWeight.bold, color: Colors.red),
-    ));
   }
 }
