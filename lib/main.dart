a1f042ec5dcdd5bbc16f3f63580cf5956ba153ad
xiaoshuaiyu1993 <xiaoshuaiyu1993@163.com>
2019-03-22 16:51:47 +0800

    MaterialApp 组件的使用

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,22 @@ void main() => runApp(MyWidget());
 class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter"),
+        ),
+        body: Hello(),
+      ),
+      // 设置APP主题
+      theme: ThemeData(primaryColor: Colors.lightBlue),
+    );
+  }
+}
+
+class Hello extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Center(
         child: Text(
       "Hello, world!",
