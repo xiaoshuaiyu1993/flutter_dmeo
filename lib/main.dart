d85a0d940c8f4e2d1bb246f7a40f36a04521d5ab
xiaoshuaiyu1993 <xiaoshuaiyu1993@163.com>
2019-03-22 16:36:28 +0800

    自定义Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {
-  runApp(Center(
-      child: Text(
-    "Hello, world!",
-    textDirection: TextDirection.ltr,
-  )));
+void main() => runApp(MyWidget());
+
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Text(
+      "Hello, world!",
+      textDirection: TextDirection.ltr,
+    ));
+  }
 }
