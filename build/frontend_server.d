8088beca4a7b2b597ed972775209e142e71bd1c8
xiaoshuaiyu1993 <xiaoshuaiyu1993@163.com>
2019-03-25 16:03:29 +0800

    列表视图的使用

---
+++ build/frontend_server.d
@@ -0,0 +1,1 @@
+frontend_server.d: /Users/tusm/flutter/bin/cache/artifacts/engine/darwin-x64/frontend_server.dart.snapshot
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './model/post.dart';
 
 void main() => runApp(MyWidget());
 
@@ -6,26 +7,45 @@ class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Flutter",
-            style: TextStyle(color: Colors.white),
-          ),
-          elevation: 0.0, // 设置导航栏下面的阴影大小
-        ),
-        body: ListView.builder(
-          itemCount: 10, // 列表总条数
-          itemBuilder: (BuildContext context, int index) {
-            return Text(
-              "$index",
-              style: TextStyle(fontSize: 25.0, color: Colors.red),
-            );
-          }, // 每条数据对应的widget
-        ),
-      ),
+      home: Home(),
       // 设置APP主题
       theme: ThemeData(primaryColor: Colors.lightBlue),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  Widget _itemBuilder(BuildContext context, int index) {
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.all(8.0),
+      child: Column(
+        children: <Widget>[
+          Image.network(posts[index].imageUrl),
+          SizedBox(height: 16.0),
+          Text(posts[index].title),
+          SizedBox(height: 8.0),
+          Text(posts[index].author),
+          SizedBox(height: 8.0),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Flutter",
+          style: TextStyle(color: Colors.white),
+        ),
+        elevation: 0.0, // 设置导航栏下面的阴影大小
+      ),
+      body: ListView.builder(
+        itemCount: posts.length, // 列表总条数
+        itemBuilder: _itemBuilder, // 每条数据对应的widget
+      ),
     );
   }
 }
