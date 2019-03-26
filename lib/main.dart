9a60f754a603c4dfa3a98a3c3420259bd92228c2
xiaoshuaiyu1993 <xiaoshuaiyu1993@163.com>
2019-03-26 10:56:56 +0800

    Tabbar组件的使用

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import './demo/listview_demo.dart';
 
 void main() => runApp(MyWidget());
 
@@ -10,24 +9,52 @@ class MyWidget extends StatelessWidget {
       home: Home(),
       debugShowCheckedModeBanner: false, // 设置导航栏上的debug标签隐藏
       // 设置APP主题
-      theme: ThemeData(primaryColor: Colors.lightBlue),
+      theme: ThemeData(primaryColor: Colors.lightBlue, splashColor: Color.fromARGB(0, 0, 0, 0)),
     );
   }
 }
 
 class Home extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Flutter",
-          style: TextStyle(color: Colors.white),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: "这是菜单按钮", // 长按按钮是显示的提示语
+            onPressed: () => debugPrint("menu button is pressed!"),
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () => debugPrint("search button is pressed!"),
+            )
+          ],
+          title: Text(
+            "Flutter",
+            style: TextStyle(color: Colors.white),
+          ),
+          elevation: 0.0, // 设置导航栏下面的阴影大小
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.sentiment_dissatisfied)),
+              Tab(icon: Icon(Icons.flight_takeoff)),
+              Tab(icon: Icon(Icons.accessibility_new))
+            ],
+          ),
         ),
-        elevation: 0.0, // 设置导航栏下面的阴影大小
+        body: TabBarView(
+          children: <Widget>[
+            Icon(Icons.sentiment_dissatisfied,
+                size: 180.0, color: Colors.greenAccent),
+            Icon(Icons.flight_takeoff, size: 180.0, color: Colors.greenAccent),
+            Icon(Icons.accessibility_new,
+                size: 180.0, color: Colors.greenAccent),
+          ],
+        ),
       ),
-      body: ListViewDemo(),
     );
   }
 }
